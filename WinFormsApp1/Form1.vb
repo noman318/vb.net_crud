67012808ceeac47a7b5b04872173df0c2424364b
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2024-03-15 14:44:24 +0530

    Completed CRUD using vb.net

--- WinFormsApp1/Form1.vb
+++ WinFormsApp1/Form1.vb
@@ -10,7 +10,7 @@ Public Class Form1
     Sub Edit()
         Try
             conn.Open()
-            cmd = New SqlCommand("UPDATE product SET(product_name=@product_name,product_price=@product_price,product_group=@product_group,product_expiry=@product_expiry,product_status=@product_status) WHERE product_no=@product_no", conn)
+            cmd = New SqlCommand("UPDATE product SET product_name=@product_name,product_price=@product_price,product_group=@product_group,product_expiry=@product_expiry,product_status=@product_status WHERE product_no=@product_no", conn)
             cmd.Parameters.AddWithValue("@product_no", CDec(product_no.Text))
             cmd.Parameters.AddWithValue("@product_name", product_name.Text)
             cmd.Parameters.AddWithValue("@product_price", CDec(product_price.Text))
@@ -21,9 +21,9 @@ Public Class Form1
 
             ' Check if rows were affected
             If rowsAffected > 0 Then
-                MessageBox.Show("Data inserted successfully.")
+                MessageBox.Show("Data Updated successfully.")
             Else
-                MessageBox.Show("No rows were inserted.")
+                MessageBox.Show("No rows were Updated.")
             End If
 
         Catch ex As Exception
@@ -32,6 +32,8 @@ Public Class Form1
             conn.Close()
         End Try
         clear()
+        data_grid_load()
+
     End Sub
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles btn_update.Click
         'update button
@@ -41,6 +43,7 @@ Public Class Form1
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btn_clear.Click
         ' clear button
+        clear()
     End Sub
 
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles product_no.TextChanged
@@ -118,9 +121,37 @@ Public Class Form1
         End Try
         clear()
     End Sub
+    Public Sub delete()
+        If MsgBox("Are u sure u want to Deletd", MsgBoxStyle.Question + vbYesNo) = vbYes Then
+            Try
+                conn.Open()
+                cmd = New SqlCommand("DELETE FROM product WHERE product_no=@product_no", conn)
+                cmd.Parameters.Clear()
+                cmd.Parameters.AddWithValue("@product_no", CDec(product_no.Text))
 
+                Dim rowsAffected As Integer = cmd.ExecuteNonQuery()
+
+                ' Check if rows were affected
+                If rowsAffected > 0 Then
+                    MessageBox.Show("Data Deleted successfully.")
+                Else
+                    MessageBox.Show("No rows were Deleted.")
+                End If
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            Finally
+                conn.Close()
+            End Try
+            clear()
+            data_grid_load()
+        Else
+            Return
+        End If
+    End Sub
     Private Sub btn_delete_Click(sender As Object, e As EventArgs) Handles btn_delete.Click
         ' delete button
+        delete()
     End Sub
 
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles product_status.CheckedChanged
@@ -134,5 +165,33 @@ Public Class Form1
         product_group.Text = DataGridView1.CurrentRow.Cells(3).Value
         product_expiry.Text = DataGridView1.CurrentRow.Cells(4).Value
         product_status.Checked = DataGridView1.CurrentRow.Cells(5).Value
+
+        product_no.ReadOnly = True
+        btn_save.Enabled = False
+    End Sub
+
+    Private Sub text_search_TextChanged(sender As Object, e As EventArgs) Handles text_search.TextChanged
+        DataGridView1.Rows.Clear()
+        Try
+            conn.Open()
+            cmd = New SqlCommand("SELECT * FROM product WHERE product_no like '%" & text_search.Text & "%' product_name like '%" & text_search.Text & "%' ", conn)
+            dr = cmd.ExecuteReader
+            While dr.Read
+                DataGridView1.Rows.Add(dr.Item("product_no"), dr.Item("product_name"), dr.Item("product_price"), dr.Item("product_group"), dr.Item("product_expiry"), Format(CBool(dr.Item("product_status"))))
+            End While
+            ' cmd = conn.CreateCommand()
+            ' cmd.CommandType = CommandType.Text
+            'cmd.CommandText = "SELECT * FROM product"
+            'cmd.ExecuteNonQuery()
+            'Dim dt As New DataTable()
+            'Dim da As New SqlDataAdapter(cmd)
+            'da.Fill(dt)
+            'DataGridView1.DataSource = dt
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Finally
+            conn.Close()
+        End Try
     End Sub
 End Class
